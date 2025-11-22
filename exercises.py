27b3f3d0b6ac3c43f4ebad748a6380058c002909
cloudy64 <zainobo1690@gmail.com>
2025-11-22 22:36:01 +0300

    done

--- exercises.py
+++ exercises.py
@@ -110,3 +110,37 @@ def list_home_town_items():
 
 # Call the function and print the result
 print('Exercise 5:', list_home_town_items())
+
+
+
+# Exercise 6: Celebrate Students
+#
+# Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings.
+# For example: ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
+
+def create_awesome_students():
+
+    students = ["zainab", "anwaar", "Nawraa"]
+
+    awesome_students = [f"{student} is awesome!" for student in students]
+    
+    return awesome_students
+
+print('Exercise 6:', create_awesome_students())
+
+
+
+# Exercise 7: Filter Foods
+#
+# Assign to a variable named foods_with_an_a the result of list comprehension that filters the foods tuple to only include food strings that contain the letter 'a'.
+# For example, if foods is a tuple of ('Taco', 'Burrito', 'Sandwich'), foods_with_an_a would be a list equal to ['Taco', 'Sandwich']
+
+def filter_foods_with_a():
+
+    foods = ("Pizza", "Burger", "Pasta", "Salad")
+
+    foods_with_an_a = [food for food in foods if "a" in food.lower()]
+    
+    return foods_with_an_a
+print('Exercise 7:', filter_foods_with_a())
+
